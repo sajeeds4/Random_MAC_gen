a7523c5894c7a9197dbb442ef3922b805597535f
Gulam Mohammed Sajeed <75039955+sajeeds4@users.noreply.github.com>
2022-08-15 12:33:06 +0530

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -2,12 +2,16 @@
 macchanger -l > vendor.txt
 var_1=$(shuf -n 1 vendor.txt | awk '{print $3}')
 var_2=$(printf '%02X:%02x:%02X' $[RANDOM%256] $[RANDOM%256] $[RANDOm%256])
-sudo macchanger -m "$var_1:$var_2" eth0
+echo Please enter your network interface one :
+read networkinterfaceone
+sudo macchanger -m "$var_1:$var_2" $networkinterfaceone
 
 #2
 sudo modprobe 8188eu
+echo Please enter your second network interface :
+read networkinterfacetwo
 service NetworkManager stop
-ifconfig wlan0 down
-macchanger -m "$var_1:$var_2" wlan0
-ifconfig wlan0 up
+ifconfig $networkinterfacetwo down
+macchanger -m "$var_1:$var_2" $networkinterfacetwo
+ifconfig $networkinterfacetwo up
 service NetworkManager start
